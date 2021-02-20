bffff700e6852d94c057c960094eb9033a498511
Ruben <15885455+RubenNL@users.noreply.github.com>
2021-02-20 16:14:53 +0100

    loading/saving of reminders

--- modules/remind.js
+++ modules/remind.js
@@ -1,9 +1,13 @@
 var CronJob = require('cron').CronJob;
 var crypto = require("crypto");
+var fs=require('fs')
 class Remind {
 	constructor(client) {
 		this.client=client;
-		this.reminders=[] //TODO uit bestand halen
+		this.reminders=JSON.parse(fs.readFileSync('reminders.json','utf8')).map(reminder=>{
+			reminder.job=this.loadReminder(reminder)
+			return reminder
+		})
 	}
 	onMessage(parts,message) {
 		action=parts.shift();
@@ -23,39 +27,57 @@ class Remind {
 				return;
 			}
 			var id = crypto.randomBytes(4).toString('hex').toUpperCase()
-			const reminder={time,text:parts[1],channel:message.channel,id}
-			this.loadReminder(reminder)
+			const reminder={time,text:parts[1],channel:message.channel.id,id}
+			reminder.job=this.loadReminder(reminder)
 			this.reminders.push(reminder)
+			message.channel.send(`configured reminder ${id} on ${reminder.time} text: \`${reminder.text}\` next: ${reminder.job.nextDate().fromNow()}`)
+			this.save();
 		} else if(action=="list") {
-			const reminders=this.reminders.filter(({channel})=>channel==message.channel)
+			const reminders=this.reminders.filter(({channel})=>channel==message.channel.id).map(reminder=>{
+				return {...reminder,next:reminder.job.nextDate().fromNow()}
+			})
 			const longestText=Math.max(7,...reminders.map(item=>item.text.length))
-			const longestTime=Math.max(4,reminders.map(item=>item.time.length))
+			const longestTime=Math.max(4,...reminders.map(item=>item.time.length))
+			const longestNext=Math.max(4,...reminders.map(item=>item.next.length))
 			console.log(longestText,longestTime)
 			let response='```\n';
-			response+=`+--------+${'-'.repeat(longestTime)}+${'-'.repeat(longestText)}+\n`
-			response+=`|id      |cron${' '.repeat(longestTime-4)}|message${' '.repeat(longestText-7)}|\n`
-			response+=`+--------+${'-'.repeat(longestTime)}+${'-'.repeat(longestText)}+\n`
+			response+=`+--------+${'-'.repeat(longestTime)}+${'-'.repeat(longestText)}+${'-'.repeat(longestNext)}+\n`
+			response+=`|id      |cron${' '.repeat(longestTime-4)}|message${' '.repeat(longestText-7)}|next${' '.repeat(longestNext-4)}|\n`
+			response+=`+--------+${'-'.repeat(longestTime)}+${'-'.repeat(longestText)}+${'-'.repeat(longestNext)}+\n`
 			reminders.forEach(reminder=>{
-				response+=`|${reminder.id}|${reminder.time}${' '.repeat(longestTime-reminder.time.length)}|${reminder.text}${' '.repeat(longestText-reminder.text.length)}|\n`
+				response+=`|${reminder.id}|${reminder.time}${' '.repeat(longestTime-reminder.time.length)}|${reminder.text}${' '.repeat(longestText-reminder.text.length)}|${reminder.next}${' '.repeat(longestNext-reminder.next.length)}|\n`
 			})
-			response+=`+--------+${'-'.repeat(longestTime)}+${'-'.repeat(longestText)}+\n`
+			response+=`+--------+${'-'.repeat(longestTime)}+${'-'.repeat(longestText)}+${'-'.repeat(longestNext)}+\n`
 			response+='```';
 			message.channel.send(response)
 		} else if(action=="delete") {
 			const id=parts.shift();
+			if(id=="all") {
+				this.reminders.forEach(reminder=>reminder.job.stop())
+				this.reminders=[]
+				message.channel.send(`deleted all reminders!!`)
+				this.save();
+				return
+			}
 			const reminder=this.reminders.find(item=>item.id==id)
 			if(!reminder) {
 				message.channel.send(`can't find reminder with id ${id}!`)
 				return
 			}
+			reminder.job.stop();
 			this.reminders=this.reminders.filter(item=>item!=reminder)
 			message.channel.send(`deleted reminder with id ${id}!`)
+			this.save();
 		}
 	}
 	loadReminder({time,text,channel}) {
-		action=()=>channel.send(text)
-		const job=new CronJob(time, action, null, true);
-		console.log(job)
+		const action=()=>this.client.channels.fetch(channel).then(channel=>channel.send(text))
+		return new CronJob(time, action, null, true);
+	}
+	save() {
+		fs.writeFileSync('reminders.json',JSON.stringify(this.reminders.map(reminder=>{
+			return {...reminder,job:undefined}
+		}),null,'\t'),'utf8')
 	}
 }
 module.exports=client=>{
